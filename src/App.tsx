35b1fb4f8e9f2880b4671a47f0726d0b27f75216
백은주 <specialguest@kakao.com>
2024-08-23 02:56:31 +0900

    feat: Page 컴포넌트 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Layout } from '@/components/Layout';
-import { ArticleSection, KeyVisualSection } from '@/components/Section';
-import { getArticles } from '@/data/article';
 import { Home } from '@/pages/Home';
+import { Page } from '@/pages/page/Page';
 import { SearchResult } from '@/pages/search/SearchResult';
 
 import '@/assets/styles/reset.scss';
@@ -10,22 +9,10 @@ import '@/assets/styles/global.scss';
 import 'virtual:stylex.css';
 
 export function App() {
-  const articles = getArticles();
-
   return (
     <Layout>
       {document.getElementById('tt-body-index') && <Home />}
-
-      {document.getElementById('tt-body-page') && (
-        <>
-          <KeyVisualSection
-            contents={[articles[0]]}
-            type="ARTICLE"
-          />
-          <ArticleSection html={articles[0].content} />
-        </>
-      )}
-
+      {document.getElementById('tt-body-page') && <Page />}
       {document.getElementById('tt-body-category') && <SearchResult />}
       {document.getElementById('tt-body-search') && <SearchResult />}
     </Layout>
---
+++ src/pages/page/Page.tsx
@@ -0,0 +1,22 @@
+import * as stylex from '@stylexjs/stylex';
+
+import { ArticleSection, KeyVisualSection } from '@/components/Section';
+import { getArticles } from '@/data/article';
+
+export function Page() {
+  const articles = getArticles();
+
+  return (
+    <div {...stylex.props(styles.container)}>
+      <KeyVisualSection
+        contents={[articles[0]]}
+        type="ARTICLE"
+      />
+      <ArticleSection html={articles[0].content} />
+    </div>
+  );
+}
+
+const styles = stylex.create({
+  container: {},
+});
