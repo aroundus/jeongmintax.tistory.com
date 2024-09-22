5b04a482656bc26bbe0f172ef15314b120d6f764
백은주 <specialguest@kakao.com>
2024-09-23 02:04:25 +0900

    fix: 슬래시를 2번 적용하는 문제

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -2,8 +2,10 @@
  * 직접 모바일 주소로 접속한 경우 리디렉션
  *
  * 블로그 관리 - 플러그인 - 구글 애널리틱스 - 측정 ID 또는 추적 ID - 다음 값을 입력합니다.
- * ');</script><script src="[redirect.js 파일 주소]"></script><script>('
+ * @see https://tesseractjh.tistory.com/209
+ *
+ * ');</script><script src="https://cdn.jsdelivr.net/gh/aroundus/jeongmintax.tistory.com@main/scripts/redirect.js"></script><script>('
  */
 if (location.pathname.startsWith('/m')) {
-  location.href = `https://${location.host}/${location.pathname.replace('/m', '')}`;
+  location.href = `https://${location.host}${location.pathname.replace('/m', '')}`;
 }
