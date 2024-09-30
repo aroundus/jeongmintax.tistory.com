451b6962643ad07c638933273ccb202be80dd123
백은주 <specialguest@kakao.com>
2024-10-01 07:53:32 +0900

    feat: 유선 상담 -> 전화 상담

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -24,13 +24,13 @@ export function Footer() {
               <strong>대표세무사</strong> 임현수
             </li>
             <li {...stylex.props(addressStyles.li)}>
+              <strong>사업자등록번호</strong> 879-36-01461
+            </li>
+            <li {...stylex.props(addressStyles.li)}>
               <strong>이메일 상담</strong> <a href="mailto:imtax8250@naver.com">imtax8250@naver.com</a>
             </li>
             <li {...stylex.props(addressStyles.li)}>
-              <strong>유선 상담</strong> 010-7643-3007
-            </li>
-            <li {...stylex.props(addressStyles.li)}>
-              <strong>사업자등록번호</strong> 879-36-01461
+              <strong>전화 상담</strong> 010-7643-3007
             </li>
             <li>
               <strong>주소</strong> 서울시 서초구 사평대로55길 82-3 301호
