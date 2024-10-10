dc37d3a6dd168f0cb3e6ebbf0c9253257c2628ff
백은주 <specialguest@kakao.com>
2024-10-11 04:28:50 +0900

    refactor: 티스토리 전역 객체 분리

---
+++ scripts/base.js
@@ -0,0 +1,79 @@
+/**
+ * 티스토리 전역 객체
+ *
+ * @description 개발 환경에서 사용합니다.
+ */
+
+window.appInfo = {
+  domain: 'tistory.com',
+  topUrl: 'https://www.tistory.com',
+  loginUrl: 'https://www.tistory.com/auth/login',
+  logoutUrl: 'https://www.tistory.com/auth/logout',
+};
+
+window.initData = {
+  user: {
+    id: 4946148,
+    loginId: 'specialguest@kakao.com',
+    name: '어라운드어스',
+  },
+};
+
+window.T = {
+  m: {},
+  admin: {},
+  blog: {},
+  ui: {},
+  util: {
+    Cookie: {},
+  },
+  config: {
+    TOP_SSL_URL: 'https://www.tistory.com',
+    PREVIEW: false,
+    ROLE: 'user',
+    PREV_PAGE: '',
+    NEXT_PAGE: '',
+    BLOG: {
+      id: 7221853,
+      name: 'jeongmintax',
+      title: '세무회계 정민',
+      isDormancy: false,
+      nickName: '임현수 세무사',
+      status: 'open',
+      profileStatus: 'normal',
+    },
+    NEED_COMMENT_LOGIN: true,
+    COMMENT_LOGIN_CONFIRM_MESSAGE:
+      '이 블로그는 로그인한 사용자에게만 댓글 작성을 허용했습니다. 지금 로그인하시겠습니까?',
+    LOGIN_URL: 'https://www.tistory.com/auth/login/?redirectUrl=https://jeongmintax.tistory.com/10',
+    DEFAULT_URL: 'https://jeongmintax.tistory.com',
+    USER: {
+      name: '임현수 세무사',
+      homepage: 'https://specialguest.tistory.com',
+      id: 4946148,
+      profileImage: 'https://t1.daumcdn.net/tistory_admin/static/manage/images/r3/default_S.png',
+    },
+    SUBSCRIPTION: {
+      status: 'none',
+      isConnected: false,
+      isPending: false,
+      isWait: false,
+      isProcessing: false,
+      isNone: true,
+    },
+    IS_LOGIN: true,
+    HAS_BLOG: true,
+    IS_SUPPORT: false,
+    IS_SCRAPABLE: false,
+    TOP_URL: 'http://www.tistory.com',
+    JOIN_URL: 'https://www.tistory.com/member/join',
+    PHASE: 'prod',
+    ROLE_GROUP: 'visitor',
+  },
+  entryInfo: {
+    entryId: 10,
+    isAuthor: false,
+    categoryId: 1246594,
+    categoryLabel: '사업자/주택임대사업자',
+  },
+};
