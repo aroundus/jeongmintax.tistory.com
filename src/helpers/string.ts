d0b40b1ca74698b40b12158f77b4a60141b8769f
백은주 <specialguest@kakao.com>
2024-11-01 07:53:53 +0900

    fix: 마침표 뒤에 공백 없이 시작하는 문자열을 포함하는 문제

--- src/helpers/string.ts
+++ src/helpers/string.ts
@@ -7,8 +7,14 @@ export function truncateWithPeriod(text: string, limit: number) {
   const lastPeriodIndex = truncatedText.lastIndexOf('.');
 
   if (lastPeriodIndex !== -1) {
-    return truncatedText.slice(0, lastPeriodIndex + 1);
+    return (
+      truncatedText
+        .slice(0, lastPeriodIndex + 1)
+        // 마침표 뒤에 공백 없이 시작하는 문자열을 잘라냅니다.
+        .replace(/\.(?!\s).*/, '.')
+        .trim()
+    );
   }
 
-  return truncatedText;
+  return truncatedText.trim();
 }
