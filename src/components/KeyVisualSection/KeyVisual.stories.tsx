353e891570b33ec18f8a1dbe8581a3c359afa410
백은주 <specialguest@kakao.com>
2024-11-01 07:53:59 +0900

    feat: 창업중소기업 세액감면

--- src/components/KeyVisualSection/KeyVisual.stories.tsx
+++ src/components/KeyVisualSection/KeyVisual.stories.tsx
@@ -26,7 +26,7 @@ export const KeyVisualStory: Story = {
     likeCount: 10,
     path: 'https://blog.naver.com/taxaroundus/223253166812',
     summary:
-      '양도와 관련하여 비과세 및 세액 감면을 검토해서 적용하면 많은 절세를 할 수 있습니다. 다음 사례의 경우 현금 보상을 받는 경우에 해당하여 10%의 세액 감면을 적용하였습니다.',
+      '양도와 관련하여 비과세 및 세액감면을 검토해서 적용하면 많은 절세를 할 수 있습니다. 다음 사례의 경우 현금 보상을 받는 경우에 해당하여 10%의 세액감면을 적용하였습니다.',
     thumbnailURL: 'https://cdn.pixabay.com/photo/2016/11/21/14/57/wheat-1845835_1280.jpg',
     title: '공익 사업을 위한 토지 수용 시 양도소득세',
     onLikeClick: () => {},
