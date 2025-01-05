095d7400a117061622399f716f680ce63a73db73
백은주 <specialguest@kakao.com>
2025-01-05 18:32:05 +0900

    chore: 배포 스크립트 작성

---
+++ scripts/deploy.ts
@@ -0,0 +1,36 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { Skin } from 'tistory-skin';
+
+(async function (directoryPath: string) {
+  const skin = new Skin(process.env.BLOG_URL, process.env.TSSESSION);
+  const filePaths = getAllFilePaths(directoryPath);
+
+  for (const filePath of filePaths) {
+    const { data } = await skin.upload(filePath);
+    console.log(data);
+  }
+})(path.join(__dirname, '../dist'));
+
+/**
+ * 주어진 디렉터리에서 모든 파일 경로를 재귀적으로 가져옵니다.
+ *
+ * @param {string} directoryPath - 검색할 디렉터리 경로
+ * @returns {string[]}
+ */
+function getAllFilePaths(directoryPath: string) {
+  let filePaths: string[] = [];
+  const dirents = fs.readdirSync(directoryPath, { withFileTypes: true });
+
+  dirents.forEach((dirent) => {
+    const fullPath = path.join(directoryPath, dirent.name);
+
+    if (dirent.isDirectory()) {
+      filePaths = filePaths.concat(getAllFilePaths(fullPath));
+    } else if (dirent.isFile()) {
+      filePaths.push(fullPath);
+    }
+  });
+
+  return filePaths;
+}
