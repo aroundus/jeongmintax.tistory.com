0aff3c42a054afe8ea04e32acc69a8f6ca41da41
백은주 <specialguest@kakao.com>
2025-01-05 18:32:10 +0900

    fix: 화면을 렌더링하지 않는 문제

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useLayoutEffect } from 'react';
+import { useLayoutEffect, useState } from 'react';
 
 import { Layout } from '@/components/Layout';
 import { Home } from '@/pages/home/Home';
@@ -11,23 +11,33 @@ import '@/assets/styles/global.scss';
 
 import 'virtual:stylex.css';
 
-const bodyType = import.meta.env.VITE_TISTORY_BODY_TYPE;
-
 export function App() {
+  const [isMounted, setIsMounted] = useState(false);
+
   useLayoutEffect(() => {
-    if (bodyType) {
-      document.querySelector('body')!.id = `tt-body-${bodyType}`;
-    } else {
-      document.querySelector('body')!.id = 'tt-body-index';
+    if (import.meta.env.DEV) {
+      const bodyType = import.meta.env.VITE_TISTORY_BODY_TYPE;
+
+      if (bodyType) {
+        document.querySelector('body')!.id = `tt-body-${bodyType}`;
+      } else {
+        document.querySelector('body')!.id = 'tt-body-index';
+      }
     }
+
+    setIsMounted(true);
   }, []);
 
   return (
-    <Layout>
-      {bodyType === undefined && <Home />}
-      {bodyType === 'category' && <Category />}
-      {bodyType === 'page' && <Article />}
-      {bodyType === 'search' && <SearchResult />}
-    </Layout>
+    <>
+      {isMounted && (
+        <Layout>
+          {document.getElementById('tt-body-index') && <Home />}
+          {document.getElementById('tt-body-category') && <Category />}
+          {document.getElementById('tt-body-page') && <Article />}
+          {document.getElementById('tt-body-search') && <SearchResult />}
+        </Layout>
+      )}
+    </>
   );
 }
