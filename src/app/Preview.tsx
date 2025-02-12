13c2642b2152177f9593db9ac148709d43af45a1
백은주 <specialguest@kakao.com>
2025-02-13 06:13:50 +0900

    fix: 관리자 페이지 스킨 편집 미리 보기 시 빈 화면을 출력하는 문제

---
+++ src/app/Preview.tsx
@@ -0,0 +1,34 @@
+import { useLayoutEffect } from 'react';
+
+import HomePage from '@/pages/(home)/HomePage';
+import ArticlePage from '@/pages/[entryID]/ArticlePage';
+import CategoryPage from '@/pages/category/CategoryPage';
+import SearchResultPage from '@/pages/search/SearchResultPage';
+import { useIsMobile } from '@/shared/lib';
+import { Footer } from '@/widgets/footer/ui';
+import { HeaderDesktop, HeaderMobile } from '@/widgets/header/ui';
+
+import 'virtual:stylex.css';
+
+export default function Preview() {
+  const isMobile = useIsMobile();
+
+  useLayoutEffect(() => {
+    if (document.getElementById('tt-body-page')) {
+      document.querySelector('body')!.id = 'tt-body-page';
+    }
+  }, []);
+
+  return (
+    <>
+      {isMobile ? <HeaderMobile /> : <HeaderDesktop />}
+      <main>
+        {document.getElementById('tt-body-index') && <HomePage />}
+        {document.getElementById('tt-body-page') && <ArticlePage />}
+        {document.getElementById('tt-body-category') && <CategoryPage />}
+        {document.getElementById('tt-body-search') && <SearchResultPage />}
+      </main>
+      <Footer />
+    </>
+  );
+}
